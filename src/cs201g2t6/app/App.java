6da6a4383f9e242dbd8ee63b7166dd5f8f32a31d
Kylie Cho <kyliecsm@gmail.com>
2021-11-01 23:54:56 +0800

    added bellmanford algo and its implementation in app

--- src/cs201g2t6/app/App.java
+++ src/cs201g2t6/app/App.java
@@ -1,23 +1,16 @@
 package cs201g2t6.app;
 
 import cs201g2t6.utils.*;
-import cs201g2t6.model.Business;
-import cs201g2t6.model.DijkstraPQ;
-import cs201g2t6.model.Graph;
-import cs201g2t6.model.Graph2;
-import cs201g2t6.model.Node;
-import cs201g2t6.model.Test;
-import cs201g2t6.model.priQueue;
-import cs201g2t6.model.priQueueComparator;
+import cs201g2t6.model.*;
 
 import java.util.*;
-import java.io.FileNotFoundException; 
+import java.io.FileNotFoundException;
 
 public class App {
     public static void main(String[] args) {
 
         /** Enter x & y coordinates of your location */ 
-        Double[] userLocation = {40.0175444, -105.28335}; 
+        Double[] userLocation = {45.5309581, -122.8237802};//{40.0175444, -105.28335}; 
 
         /** Find restaurants within this distance */ 
         double maxDistance = 2.0; 
@@ -26,23 +19,26 @@ public class App {
         double minRating = 4.0; 
         
         try {
-            List<Business> allBusinessList = FileReader.readFile("/Users/akshayanatarajan/Desktop/Y2S1/CS201/PROJ/code/26OCTtrial/cs201project/data/business.csv");
+            List<Business> allBusinessList = FileReader.readFile("data/business.csv");
             List<Business> nearbyBusinessList = FilterBusinesses.getBusinessesNearby(allBusinessList, userLocation, maxDistance); 
             //List<Business> nearbyRestaurantsList = FilterBusinesses.getRestaurantsOnly(nearbyBusinessList); 
             //List<Business> nearbyHighestRatedRestaurantsList = FilterBusinesses.getHighestRatedRestaurants(nearbyRestaurantsList, minRating); 
-
             //System.out.println(nearbyHighestRatedRestaurantsList);
 
-        // aks implementation
+             // aks implementation
 
             // MAKE USERLOCATION A BIZ SET EVERYTHING BUT LONG , LAT TO NULL 
-            Business user = new Business("aks","aks", userLocation[0], userLocation[1],null, 0); 
+            Business user = new Business(null, "user", userLocation[0], userLocation[1], null, 0); 
             // add user into biz list
             nearbyBusinessList.add(user); 
 
             Graph2 graph = new Graph2(nearbyBusinessList.size(), nearbyBusinessList);
-            DijkstraPQ dijkstraPQ = new DijkstraPQ(graph, user); 
-            dijkstraPQ.doDijkstraPQ();
+            // DijkstraPQ dijkstraPQ = new DijkstraPQ(graph, user); 
+            // dijkstraPQ.doDijkstraPQ();
+
+            // testing bellman ford
+            BellmanFord bellmanFord = new BellmanFord(graph, user);
+            bellmanFord.doBellmanFord();
             
         } catch (FileNotFoundException e) {
             System.out.println("File does not exist!"); 
